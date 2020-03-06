c30f4e61cb91e4e401df42745afd1b46689062a6
igorssnaidermans <igors.snaidermans@icloud.com>
2020-03-06 18:36:26 +0200

    fixed bug - stickies were draggable after one click

--- src/scripts.js
+++ src/scripts.js
@@ -28,7 +28,7 @@ function addNewStickie(){
         $("nav").after("<div class=\"stickie-container stickie-blue\" id=\""+stickieIdCounter+"\">\n" +
             "        <div class=\"trash stickie-blue-title\"><i class=\"fa fa-ban\" id=\"stickie-control-icon\"></i></div>\n" +
             "        <div class=\"stickie-title stickie-blue-title\">Title</div>\n" +
-            "        <div class=\"move stickie-blue-title\" id=\"stickieMoveId "+stickieIdCounter+"\" onclick=\"setItemToDrag("+stickieIdCounter+")\"><i class=\"fa fa-arrows\"\n" +
+            "        <div class=\"move stickie-blue-title\" id=\"stickieMoveId "+stickieIdCounter+"\"><i class=\"fa fa-arrows\"\n" +
             "                                                                                  id=\"stickie-control-icon\"></i></div>\n" +
             "        <div class=\"content\"><p id=\"stickie-content-text\">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec\n" +
             "            pellentesque pellentesque ligula.</p></div>\n" +
@@ -37,7 +37,7 @@ function addNewStickie(){
         $("nav").after("<div class=\"stickie-container stickie-yellow\" id=\""+stickieIdCounter+"\">\n" +
             "        <div class=\"trash stickie-yellow-title\"><i class=\"fa fa-ban\" id=\"stickie-control-icon\"></i></div>\n" +
             "        <div class=\"stickie-title stickie-yellow-title\">Title</div>\n" +
-            "        <div class=\"move stickie-yellow-title\" id=\"stickieMoveId "+stickieIdCounter+"\" onclick=\"setItemToDrag("+stickieIdCounter+")\"><i class=\"fa fa-arrows\"\n" +
+            "        <div class=\"move stickie-yellow-title\" id=\"stickieMoveId "+stickieIdCounter+"\"><i class=\"fa fa-arrows\"\n" +
             "                                                                                  id=\"stickie-control-icon\"></i></div>\n" +
             "        <div class=\"content\"><p id=\"stickie-content-text\">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec\n" +
             "            pellentesque pellentesque ligula.</p></div>\n" +
@@ -46,53 +46,47 @@ function addNewStickie(){
         $("nav").after("<div class=\"stickie-container stickie-black\" id=\""+stickieIdCounter+"\">\n" +
             "        <div class=\"trash stickie-black-title\"><i class=\"fa fa-ban\" id=\"stickie-control-icon\"></i></div>\n" +
             "        <div class=\"stickie-title stickie-black-title\">Title</div>\n" +
-            "        <div class=\"move stickie-black-title\" id=\"stickieMoveId "+stickieIdCounter+"\" onclick=\"setItemToDrag("+stickieIdCounter+")\"><i class=\"fa fa-arrows\"\n" +
+            "        <div class=\"move stickie-black-title\" id=\"stickieMoveId "+stickieIdCounter+"\"><i class=\"fa fa-arrows\"\n" +
             "                                                                                  id=\"stickie-control-icon\"></i></div>\n" +
             "        <div class=\"content\"><p id=\"stickie-content-text\">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec\n" +
             "            pellentesque pellentesque ligula.</p></div>\n" +
             "    </div>");
     }
+
     stickieCounter();
+    dragElement(document.getElementById(stickieIdCounter.toString()), stickieIdCounter);
     stickieIdCounter++;
 }
 
-function setItemToDrag(itemId){
-    itemIdToDrag=itemId;
-    dragElement(document.getElementById(itemId));
-}
 
-function dragElement(elmnt) {
+
+function dragElement(elmnt, itemId) {
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
 
-    document.getElementById("stickieMoveId " + itemIdToDrag).onmousedown = dragMouseDown;
+    document.getElementById("stickieMoveId " + itemId).onmousedown = dragMouseDown;
 
 
     function dragMouseDown(e) {
         e = e || window.event;
         e.preventDefault();
-        // get the mouse cursor position at startup:
         pos3 = e.clientX;
         pos4 = e.clientY;
         document.onmouseup = closeDragElement;
-        // call a function whenever the cursor moves:
         document.onmousemove = elementDrag;
     }
 
     function elementDrag(e) {
         e = e || window.event;
         e.preventDefault();
-        // calculate the new cursor position:
         pos1 = pos3 - e.clientX;
         pos2 = pos4 - e.clientY;
         pos3 = e.clientX;
         pos4 = e.clientY;
-        // set the element's new position:
         elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
         elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
     }
 
     function closeDragElement() {
-        /* stop moving when mouse button is released:*/
         document.onmouseup = null;
         document.onmousemove = null;
     }
